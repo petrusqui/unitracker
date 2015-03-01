9c7d2ffee89ca86dbaf79cae0cc19b9c17716487
Petru Rares Sincraian <psincraian@gmail.com>
2015-03-01 10:56:13 +0100

    Changed some setings of the button

--- app/src/main/java/com/besttracker/unitracker2/MainActivity.java
+++ app/src/main/java/com/besttracker/unitracker2/MainActivity.java
@@ -1,7 +1,6 @@
 package com.besttracker.unitracker2;
 
 import android.app.Activity;
-import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.database.Cursor;
